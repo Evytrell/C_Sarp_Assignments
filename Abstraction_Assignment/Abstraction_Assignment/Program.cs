5528905c6e31711aecf358ce122c20b190573afa
Edna Nwokedi <105814211+Evytrell@users.noreply.github.com>
2022-08-12 00:13:28 +0100

    Add files via upload

---
+++ Abstraction_Assignment/Abstraction_Assignment/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Abstraction_Assignment
+{
+
+    //using interface to show abstraction
+
+     public interface ISchoolRules
+    {
+
+        public void Punctual();
+
+        public void Neat();
+
+
+        static void Main(string[] args)
+        {
+             
+            
+        }
+        class Teachers : ISchoolRules
+        {
+            void ISchoolRules.Neat()
+            {
+                Console.WriteLine("I always ensure my clothes are clean and well ironed");
+            }
+
+            void ISchoolRules.Punctual()
+            {
+               Console.WriteLine("I leave my house early so I can get to school on time"); 
+            }
+        }
+
+        class Students : ISchoolRules
+        {
+            void ISchoolRules.Neat()
+            {
+                Console.WriteLine("My uniform is always clean and ironed");
+            }
+
+            void ISchoolRules.Punctual()
+            {
+                Console.WriteLine(" I always like being punctual");
+            }
+        }
+     }
+    
+}
