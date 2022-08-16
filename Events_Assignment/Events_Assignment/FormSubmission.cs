dbf7d2852e07d9d1e07bcd1610684488cbc2a7d0
Edna Nwokedi <105814211+Evytrell@users.noreply.github.com>
2022-08-16 08:20:18 +0100

    Add files via upload

---
+++ Events_Assignment/Events_Assignment/FormSubmission.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events_Assignment
+{
+    internal class FormSubmission    //publisher
+    {
+        public delegate void AlertHandler();
+        public event AlertHandler Response;
+
+        /// <summary>
+        /// when submit button is clicked when filling a form
+        /// </summary>
+        public void ButtonClicked()
+
+        {          
+            Console.WriteLine("Submission in progress");
+
+            Thread.Sleep(10000);
+
+            ResponseReceived();
+        }
+
+        /// <summary>
+        /// This method invokes the event
+        /// </summary>
+        protected virtual void ResponseReceived()
+        {
+            Response?.Invoke(); //raise event
+        }
+    }
+
+    public class HumanResources  //subscriber
+    {
+
+
+        /// <summary>
+        /// This method notifies the human resource team that a form has been submitted
+        /// </summary>
+        public void SubmissionProcesses()
+        {
+            var formSub = new FormSubmission();
+            formSub.Response += FormSubmissionTasks;
+            formSub.ButtonClicked();
+        }
+
+        public void FormSubmissionTasks()
+        {
+            Console.WriteLine("Form submitted successfully");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Events_Assignment/Events_Assignment/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Events_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HumanResources hr = new HumanResources();
+            hr.SubmissionProcesses();
+        }
+    }
+}
