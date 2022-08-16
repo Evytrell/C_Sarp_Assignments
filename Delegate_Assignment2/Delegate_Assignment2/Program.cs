7d9f235fb97fa99487c1edd1dde27d95b352bd2a
Edna Nwokedi <105814211+Evytrell@users.noreply.github.com>
2022-08-16 08:23:29 +0100

    Add files via upload

---
+++ Delegate_Assignment2/Delegate_Assignment2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Delegate_Assignment2
+{
+    internal class Program
+    {
+        public delegate bool Pred(XClass x);
+        
+        static void Main(string[] args)
+        {
+            var likelyUsername = "Evelyn445";
+
+            Pred classx = (XClass x) =>
+            {
+                if (x.UserName == likelyUsername)  // this line is to check if username is equal to likely username
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            };
+
+        }
+    }
+    class XClass
+    {
+        public string LastName { get; set; }    
+        public string FirstName { get; set; }   
+        public string UserName { get; set; }    
+
+    }
+}
